9a5d007ab68c454f5311aa3f3e13a3f41d5fd801
sesteban951 <sesteban@caltech.edu>
2024-11-14 17:26:45 -0800

    got flight and ground props working. Need booleans and coordinate transforms

--- parallelization/slip_prop_jax.py
+++ parallelization/slip_prop_jax.py
@@ -16,7 +16,7 @@ from flax import struct        # enforcing immutable data structures
 # STRUCTS
 ######################################################################################
 
-# data class to hold the system parameters
+# system parameters data class
 @struct.dataclass
 class slip_params:
     m:  float    # mass, [kg]
@@ -31,6 +31,18 @@ class slip_params:
     umin: float  # min control input, [N]
     dt:  float   # integration time step, [s]
 
+# normal distribution data class
+@struct.dataclass
+class normal_dist:
+    mean: jnp.array
+    cov: jnp.array
+
+# uniform distribution data class
+@ struct.dataclass
+class uniform_dist:
+    lower: jnp.array
+    upper: jnp.array
+
 ######################################################################################
 # DYNAMICS
 ######################################################################################
@@ -38,7 +50,7 @@ class slip_params:
 # SLIP flight forward propagation
 @partial(jit, static_argnums=(2))
 def slip_flight_fwd_prop(x0: jnp.array, 
-                         alpha: float, 
+                         distribution: uniform_dist,
                          params: slip_params):
     '''
     Simulate the flight phase of the SLIP model
@@ -60,6 +72,9 @@ def slip_flight_fwd_prop(x0: jnp.array,
 
     # compute the time until apex and impact, (vz(t) = vz_0 - g*t)
     t_apex = vz / params.g
+
+    # compute the angle of the leg at apex
+    alpha = 0 # TODO: sample this from a distribution
 
     # compute the impact time,  (pz(t) = pz_0 + vz_0*t - 0.5*g*t^2)
     #                        => 0 = (pz_0 - pz_impact) + vz_0*t_impact - 0.5*g*t_impact^2
@@ -114,19 +129,63 @@ def slip_ground_dynamics(x: jnp.array,
     return xdot
 
 # SLIP ground forward propagation
-# @partial(jit, static_argnums=(2))
+@partial(jit, static_argnums=(1))
 def slip_ground_fwd_prop(x0: jnp.array,
-                         u: jnp.array,
                          params: slip_params):
-        '''
-        Simulate the ground phase of the SLIP model
-        '''
-        # unpack the initial state
-        r = x0[0]         # leg length
-        theta = x0[1]     # leg angle
-        r_dot = x0[2]     # leg length rate
-        theta_dot = x0[3] # leg angle rate
+    '''
+    Simulate the ground phase of the SLIP model
+    '''
 
+    # RK2 integration
+    def _RK2_step(carry, i):
+
+        # TODO: define some controller here
+        u = 0.0
+
+        # do the RK2 step
+        x_prev = carry
+        f1 = slip_ground_dynamics(x_prev, u, params)
+        f2 = slip_ground_dynamics(x_prev + 0.5 * params.dt * f1, u, params)
+        x_next = x_prev + params.dt * f2
+
+        # compute relevant quantities
+        r = x_next[0]
+        theta = x_next[1]
+        r_dot = x_next[2]
+        theta_dot = x_next[3]
+        px = r * jnp.sin(theta) 
+        pz = r * jnp.cos(theta)
+        vx = r_dot * jnp.sin(theta) + r * theta_dot * jnp.cos(theta)
+        vz = r_dot * jnp.cos(theta) - r * theta_dot * jnp.sin(theta)
+        
+        # vectors to use for the next iteration
+        l_leg = jnp.array([px, pz])
+        v_com = jnp.array([vx, vz])
+
+        # booleans
+        leg_uncompressed = (r >= params.l0)
+        ortho_velocity = (jnp.dot(v_com, l_leg) >= 0)
+        take_off = leg_uncompressed & ortho_velocity
+        
+        jax.debug.print("Iteration: {}", i)
+        # Use jax.debug.print for printing in JAX
+        # jax.debug.print("leg_uncompressed: {}", leg_uncompressed)
+        # jax.debug.print("ortho_velocity: {}", ortho_velocity)    
+        jax.debug.print("take_off: {}", take_off)
+        
+        return x_next, x_next
+
+    # iterate over the RK2 steps
+    N = 200
+    x_last, x_t = lax.scan(_RK2_step, x0, jnp.arange(1, N))
+
+    # append the initial state to the trajectory
+    x_t = jnp.vstack([x0, x_t])
+
+    # take off time
+    t_to = N * params.dt
+
+    return t_to, x_t
 
 ######################################################################################
 # TESTING
@@ -187,3 +246,15 @@ if __name__ == "__main__":
     print(f"t_impact: {t_impact}")
     print(f"x_apex: {x_apex}")
     print(f"x_impact: {x_impact}")
+
+    # ground forward propagation
+    x0 = jnp.array([1.0,  # r
+                    0.0,  # theta
+                    0.0,  # r_dot
+                    0.25]) # theta_dot
+    t_to, x_t = slip_ground_fwd_prop(x0, sys_params)
+
+    print(x_t.shape)
+
+    # print(f"t_to: {t_to}")
+    # print(f"x_t: {x_t}")
