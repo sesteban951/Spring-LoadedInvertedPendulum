b31401efbcc58231ce8fd1785550ae002364e222
sesteban951 <sesteban@caltech.edu>
2024-12-06 17:26:29 -0800

    sampling leg configurations

--- matlab/misc/SpringMassDamper_sim.m
+++ matlab/misc/SpringMassDamper_sim.m
@@ -4,102 +4,111 @@
 clc; clear all; close all;
 
 % system parameters
-params.m = 1;  % mass
-params.k = 50;  % spring constant
-params.b = 5;  % damping constant
-params.g = 9.81;  % gravity
-params.l0 = 0.5;  % nominal length
+sys.m = 1;     % mass
+sys.k = 50;    % spring constant
+sys.b = 5;     % damping constant
+sys.g = 9.81;  % gravity
+sys.l0 = 0.4;  % nominal length
+
+% mpc parameters
+mpc.K = 100;     % number of rollouts
+mpc.N = 100;     % number of time steps
+mpc.dt = 0.01;   % time step
+mpc.mu = 0;      % mean
+mpc.sigma = 0.1; % standard deviation
 
 % simulation parameters
-params.dt = 0.01;
-
-% simulation parameters
-t_max = 3.0;
-tspan = 0:params.dt:t_max;
+tspan = 0:mpc.dt:mpc.dt*(mpc.N-1);
 
 % initial conditions
 x0 = [0.5; % inital position 
       0.0]; % initial velocity
 
-% sample a control trajectory
-mu = params.l0;
-sigma = 0.05;
-u_rand = normrnd(mu, sigma, length(tspan), 1);
-u_t = [tspan', u_rand];
 
-% simulate the system
-[t, x] = ode45(@(t, x) dynamics(t, x, u_t, params), tspan, x0);
+% % plot the results
+% figure('Name', 'Spring-Mass-Damper Simulation');
 
-% plot the results
-figure('Name', 'Spring-Mass-Damper Simulation');
+% subplot(3, 2, 1);
+% plot(t, x(:, 1), 'LineWidth', 2);
+% xlabel('Time [s]');
+% ylabel('Position [m]');
+% grid on;
 
-subplot(3, 2, 1);
-plot(t, x(:, 1), 'LineWidth', 2);
-xlabel('Time [s]');
-ylabel('Position [m]');
-grid on;
+% subplot(3, 2, 3);
+% plot(t, x(:, 2), 'LineWidth', 2);
+% xlabel('Time [s]');
+% ylabel('Velocity [m/s]');
+% grid on;
 
-subplot(3, 2, 3);
-plot(t, x(:, 2), 'LineWidth', 2);
-xlabel('Time [s]');
-ylabel('Velocity [m/s]');
-grid on;
+% subplot(3, 2, 5);
+% plot(u_t(:,1),u_t(:,2), 'LineWidth', 2);
+% xlabel('Time [s]');
+% ylabel('Control [N]');
+% grid on;
 
-subplot(3, 2, 5);
-plot(u_t(:,1),u_t(:,2), 'LineWidth', 2);
-xlabel('Time [s]');
-ylabel('Control [N]');
-grid on;
+% subplot(3, 2, [2,4,6]);
+% hold on;
+% yline(0); 
+% yline(0.75, '--');
+% yline(-0.75, '--');
+% ylabel('Position [m]');
+% axis equal; grid on;
 
-subplot(3, 2, [2,4,6]);
-hold on;
-yline(0); 
-yline(0.75, '--');
-yline(-0.75, '--');
-ylabel('Position [m]');
-axis equal; grid on;
+% tic;
+% ind = 1;
+% while 1==1
 
-tic;
-ind = 1;
-while 1==1
-
-    % draw the mass spring damper system
-    pole = plot([0, 0], [0, x(ind, 1)], 'k', 'LineWidth', 2);
+%     % draw the mass spring damper system
+%     pole = plot([0, 0], [0, x(ind, 1)], 'k', 'LineWidth', 2);
     
-    box_center = [0; x(ind, 1)];
-    box = rectangle('Position', [box_center(1)-0.1, box_center(2)-0.1, 0.2, 0.2], 'Curvature', 0.1, 'FaceColor', 'r');
+%     box_center = [0; x(ind, 1)];
+%     box = rectangle('Position', [box_center(1)-0.1, box_center(2)-0.1, 0.2, 0.2], 'Curvature', 0.1, 'FaceColor', 'r');
     
-    drawnow;
+%     drawnow;
 
-    % put the time in the title
-    msg = sprintf('Time: %.2f sec', t(ind));
-    title(msg);
+%     % put the time in the title
+%     msg = sprintf('Time: %.2f sec', t(ind));
+%     title(msg);
 
-    % wait until the next time step
-    while toc < t(ind+1)
-        % end
-    end
+%     % wait until the next time step
+%     while toc < t(ind+1)
+%         % end
+%     end
 
-    % increment the index
-    if ind+1 >= length(t)
-        break;
-    else
-        ind = ind + 1;
-        delete(pole);
-        delete(box);
-    end
+%     % increment the index
+%     if ind+1 >= length(t)
+%         break;
+%     else
+%         ind = ind + 1;
+%         delete(pole);
+%         delete(box);
+%     end
+% end
+
+% perform a rollout
+function [x_t, u_t] = rollout(x0, u_t, sys)
+
+    % simulate the system
+    [t, x] = ode45(@(t, x) dynamics(t, x, u_t, sys), tspan, x0);
+
+    % return the state and control trajectories
+    x_t = x;
+    u_t = u_t;
+
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% DYNAMICS AND CONTROL
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% solution to a linear system 
-function xdot = dynamics(t, x, u_t, params)
+% dynamics of the linear system
+function xdot = dynamics(t, x, u_t, sys)
 
     % system parameters
-    k = params.k;
-    m = params.m;
-    b = params.b;
-    g = params.g;
+    k = sys.k;
+    m = sys.m;
+    b = sys.b;
+    g = sys.g;
 
     % define the linear system matrices
     A = [0, 1; 
@@ -107,10 +116,10 @@ function xdot = dynamics(t, x, u_t, params)
     B = [0; 
          1/m];
     C = [0; 
-         (k/m)*params.l0-g];
+         (k/m)*sys.l0-g];
 
     % get the control action
-    u = get_control(t, x, u_t, params);
+    u = get_control(t, x, u_t, sys);
 
     % define the dynamics
     xdot = A*x + B*u + C;
@@ -118,36 +127,44 @@ function xdot = dynamics(t, x, u_t, params)
 end
 
 % get the piecewise constant control action
-function u = get_control(t, x, u_t, params)
+function u = get_control(t, x, u_t, sys)
 
-    % % extract the time and control values
-    % t_vals = u_t(:, 1);
-    % u_vals = u_t(:, 2);
+    % extract the time and control values
+    t_vals = u_t(:, 1);
+    u_vals = u_t(:, 2);
 
-    % % find which intervalm we are currently in
-    % ind = find(t_vals <= t, 1, 'last');
+    % find which intervalm we are currently in
+    ind = find(t_vals <= t, 1, 'last');
     
-    % % in an interval
-    % if ind+1 <= length(t_vals)
-    %     u0 = u_vals(ind);
-    %     uf = u_vals(ind+1);
-    %     t0 = t_vals(ind);
-    %     tf = t_vals(ind+1);
-    %     u = u0 + (uf-u0)/(tf-t0)*(t-t0);
-    % % beyond the last interval
-    % else
-    %     u = u_vals(end);
-    % end
-
-    p_des = 0.2;
-    v_des = 0.0;
-    
-
-    u_ff = params.m * (-(params.k/params.m) * params.l0 + params.g);
-    kp = 0.0;
-    kd = 0.0;
-    % u = kp*(p_des - x(1)) + kd*(v_des - x(2)) + u_ff;
-    u = 0;
+    % linear interpolation
+    if ind+1 <= length(t_vals) % t inside an interval
+        u0 = u_vals(ind);
+        uf = u_vals(ind+1);
+        t0 = t_vals(ind);
+        tf = t_vals(ind+1);
+        u = u0 + (uf-u0)/(tf-t0)*(t-t0);
+    else                       % t beyond last interval
+        u = u_vals(end);
+    end
 
 end
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% STOCHASTIC
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% get weiner process trajectory
+function w_t = get_random_process(sys, mpc)
+
+    % trajecotry info
+    dt = sys.dt;
+    N = mpc.N;
+    mu = mpc.mu;
+    sigma = mpc.sigma;
+
+    
+
+    
+    
+
+end
---
+++ matlab/misc/leg_sampling.m
@@ -0,0 +1,58 @@
+clear all; close all; clc; 
+
+% sample 
+r = 1.0;
+
+% unformly sample angles
+N = 1000;
+
+% for uniform sampling
+lb = [-15, -45]; % lower bound
+ub = [15, 45];   % upper bound
+angles = [unifrnd(lb(1), ub(1), 1, N);
+unifrnd(lb(2), ub(2), 1, N)]';
+
+% for normal sampling
+mu = [0; 0];            % mean
+sigma = diag([15^2, 15^2]); % covaraince
+angles = mvnrnd(mu, sigma, N);
+
+% convert to radians
+angles = angles * pi/180;        % rad 
+
+% compute the foot positions
+l_zero = [0; 0; -r];
+l_final = zeros(N, 3);
+for i = 1:N
+    phi = angles(i, 1);
+    psi = angles(i, 2);
+    
+    R_phi = [1, 0, 0;
+             0, cos(phi), -sin(phi);
+             0, sin(phi), cos(phi)];
+    R_psi = [cos(psi),  0, sin(psi);
+             0,         1, 0;
+             -sin(psi), 0, cos(psi)];
+    
+    l_final(i, :) = R_psi * R_phi * l_zero;
+end
+
+% plot the unit sphere
+figure;
+hold on;
+axis equal;
+
+% plot the x, y, z axes
+quiver3(0, 0, 0, 1, 0, 0, 'r', 'LineWidth', 2);
+quiver3(0, 0, 0, 0, 1, 0, 'g', 'LineWidth', 2);
+quiver3(0, 0, 0, 0, 0, 1, 'b', 'LineWidth', 2);
+
+% sphere
+[X_sphere, Y_sphere, Z_sphere] = sphere;
+surf(X_sphere, Y_sphere, Z_sphere, 'FaceAlpha', 0.1, 'EdgeAlpha', 0.1);
+view(45,30);
+
+% plot the leg positions
+plot3([0, 0], [0, 0], [0, 0], 'k.', 'MarkerSize', 20);
+plot3([0, 0], [0, 0], [0, -r], 'k--', 'LineWidth', 2);
+plot3(l_final(:, 1), l_final(:, 2), l_final(:, 3), 'r.', 'MarkerSize', 10);
