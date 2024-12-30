92ede3b9c24da2058445e0a31c44207494d88d13
sesteban951 <sesteban@caltech.edu>
2024-12-29 19:48:36 -0800

    added MDROM cartesian to polar conversion

--- matlab/leg/leg_kinematics.py
+++ matlab/leg/leg_kinematics.py
@@ -8,35 +8,42 @@ import math
 
 # TODO: try for 3D. shoudl be easy to do
 
-if __name__ == '__main__':
+p_com = np.array([0, 2]).reshape(2, 1)
+v_com = np.array([-1, 0]).reshape(2, 1)
 
-    p_com = np.array([0, 2]).reshape(2, 1)
-    v_com = np.array([-1, 0]).reshape(2, 1)
+p_foot = np.array([1, 0]).reshape(2, 1)
+v_foot = np.array([0, 0]).reshape(2, 1)
 
-    p_foot = np.array([1, 0]).reshape(2, 1)
-    v_foot = np.array([0, 0]).reshape(2, 1)
+# leg length state
+r_vec = p_foot - p_com
+rdot_vec = v_foot - v_com
+r = np.linalg.norm(r_vec)
+r_unit = r_vec / r
+rdot = -v_com.T @ r_unit
 
-    # leg length state
-    r_vec = p_foot - p_com
-    rdot_vec = v_foot - v_com
-    r = np.linalg.norm(r_vec)
-    r_unit = r_vec / r
-    rdot = -v_com.T @ r_unit
+# angle state
+r_x = r_vec[0]
+r_z = r_vec[1]
+theta = -np.arctan2(r_x, -r_z)
+# theta = -math.atan(r_x, -r_z)
+rdot_x = rdot_vec[0]
+rdot_z = rdot_vec[1]
+thetadot = (r_z * rdot_x - r_x * rdot_z) / r
 
-    # angle state
-    theta = -np.arctan2(r_vec[0], -r_vec[1])
-    # theta = -math.atan(r_vec[0]/-r_vec[1])
-    r_x = r_vec[0]
-    r_z = r_vec[1]
-    rdot_x = rdot_vec[0]
-    rdot_z = rdot_vec[1]
-    thetadot = (r_z * rdot_x - r_x * rdot_z) / r
+# axtract from numpy array
+r = r
+rdot = rdot[0][0]
+theta = theta[0]
+thetadot = thetadot[0]
 
-    print('*' * 50)
-    print('r: ', r)
-    print('rdot: ', rdot)
-    print('angle: ', theta * (180/np.pi))
-    print('thetadot: ', thetadot * (180/np.pi))
+x_polar = np.array([r, theta, rdot, thetadot]).reshape(4, 1)
+print('x_polar: ', x_polar)
+
+print('*' * 50)
+print('r: ', r)
+print('rdot: ', rdot)
+print('angle: ', theta * (180/np.pi))
+print('thetadot: ', thetadot * (180/np.pi))
 
 plt.figure()
 plt.grid()
@@ -46,7 +53,9 @@ plt.plot([p_com[0], p_foot[0]], [p_com[1], p_foot[1]], 'k')
 plt.quiver(p_com[0], p_com[1], v_com[0], v_com[1], angles='xy', scale_units='xy', scale=1, color='r')
 plt.plot(0, 0, 'k+', markersize=25)
 plt.plot(p_com[0], p_com[1], 'ko', markersize=15)
-plt.title('angle: ' + str(theta * (180/np.pi)))
+
+msg_title = 'r: ' + str(r) + '\nrdot: ' + str(rdot) + '\ntheta: ' + str(theta * (180/np.pi)) + '\nthetadot: ' + str(thetadot * (180/np.pi))
+plt.title(msg_title)
 
 plt.show()
 
--- prototype/MDROM.py
+++ prototype/MDROM.py
@@ -144,6 +144,11 @@ class MDROM:
             a_com = a_left + a_right + np.array([[0], [-g]])
             xdot = np.vstack((v_com, a_com))
         
+        # get the polar leg states
+        # x_left, x_right = self.update_leg_state(x_com, p_left, p_right, u, d)
+        # print('x_left: ', x_left)
+        # print('x_right: ', x_right)
+
         return xdot    
 
     # interpolate the control input
@@ -191,7 +196,15 @@ class MDROM:
         Tx = np.arange(0, N) * dt
         Tu = np.arange(0, N-1) * dt
         xt_com = np.zeros((4, N))
+        xt_left = np.zeros((4, N))
+        xt_right = np.zeros((4, N))
         xt_com[:, 0] = x0_com.reshape(4) 
+
+        # get the feet positions
+        p_left = np.array([[x0_left[0]],  # position of the left leg
+                           [x0_left[1]]]).reshape(2, 1)
+        p_right = np.array([[x0_right[0]],  # position of the right leg
+                            [x0_right[1]]]).reshape(2, 1)
 
         # RK3 integration
         xk_com = x0_com
@@ -229,12 +242,17 @@ class MDROM:
 
             # take the step
             xk_com = xk_com + (dt/6) * (f1 + 4*f2 + f3)
-            xt_com[:, i+1] = xk_com.reshape(4)
+            x_left, x_right = self.update_leg_state(xk_com, p_left, p_right, u1, dk)
 
-        return Tx, xt_com
+            # store the states
+            xt_com[:, i+1] = xk_com.reshape(4)
+            xt_left[:, i+1] = x_left.reshape(4)
+            xt_right[:, i+1] = x_right.reshape(4)
+
+        return Tx, xt_com, xt_left, xt_right
     
     # update leg polar state
-    def update_leg_state(self, x_com, x_left_cart, x_right_cart):
+    def update_leg_state(self, x_com, p_left, p_right, u, D):
 
         # unpack COM state
         p_com = np.array([[x_com[0]],  
@@ -242,19 +260,125 @@ class MDROM:
         v_com = np.array([[x_com[2]],
                           [x_com[3]]]).reshape(2, 1)
         
-        # compute the leg states
-        p_left = np.array([[x_left_cart[0]],
-                           [x_left_cart[1]]]).reshape(2, 1)
-        p_right = np.array([[x_right_cart[0]],
-                            [x_right_cart[1]]]).reshape(2, 1) 
-        
-        # compute polar states
-        rL_vec = p_left - p_com
-        r = np.linalg.norm(rL_vec)
-        theta = 
+        # in flight (state governed by kinemaitc input)
+        if D == 'F':
 
+            # TODO: apply the control input, U
+            u = None
 
+            # pack into state vectors
+            x_left = np.array([[self.l0],
+                               [0.0],
+                               [0.0],
+                               [0.0]])
+            x_right = np.array([[self.l0],
+                                [0.0],
+                                [0.0],
+                                [0.0]])
         
+        # in left support (leg state governed by dynamics)
+        if D == 'L':
+            # compute relevant vectors
+            r_vec_L = p_left - p_com
+            r_hat_L = r_vec_L / np.linalg.norm(r_vec_L)
+            rdot_vec_L = -v_com
+
+            r_x_L = r_vec_L[0]
+            r_z_L = r_vec_L[1]
+            rdot_x_L = rdot_vec_L[0]
+            rdot_z_L = rdot_vec_L[1]
+
+            # compute the left leg state in polar coordinates
+            rL = np.linalg.norm(r_vec_L)
+            rdot_L = -v_com.T @ r_hat_L
+            theta_L = -np.arctan2(r_x_L, -r_z_L)
+            thetadot_L = (r_z_L * rdot_x_L - r_x_L * rdot_z_L) / rL
+
+            # TODO: apply the control input, U
+            u = None
+
+            # pack into state vectors
+            x_left = np.array([[rL],
+                               [theta_L],
+                               [rdot_L],
+                               [thetadot_L]])
+            x_right = np.array([[self.l0],
+                                [0.0],
+                                [0.0],
+                                [0.0]])
+
+        # in right support (leg state governed by dynamics)
+        if D == 'R':
+            # compute relevant vectors
+            r_vec_R = p_right - p_com
+            r_hat_R = r_vec_R / np.linalg.norm(r_vec_R)
+            rdot_vec_R = -v_com
+
+            r_x_R = r_vec_R[0]
+            r_z_R = r_vec_R[1]
+            rdot_x_R = rdot_vec_R[0]
+            rdot_z_R = rdot_vec_R[1]
+
+            # compute the right leg state in polar coordinates
+            rR = np.linalg.norm(r_vec_R)
+            rdot_R = -v_com.T @ r_hat_R
+            theta_R = -np.arctan2(r_x_R, -r_z_R)
+            thetadot_R = (r_z_R * rdot_x_R - r_x_R * rdot_z_R) / rR
+
+            # TODO: apply the control input, U
+            u = None
+
+            # pack into state vectors
+            x_left = np.array([[self.l0],
+                               [0.0],
+                               [0.0],
+                               [0.0]])
+            x_right = np.array([[rR],
+                                [theta_R],
+                                [rdot_R],
+                                [thetadot_R]])
+
+        # in double support (leg state governed by dynamics)
+        if D == 'D':
+            # compute relevant vectors
+            r_vec_L = p_left - p_com
+            r_vec_R = p_right - p_com
+
+            rdot_vec_L = -v_com  # assuming foot is pinned and not slipping
+            rdot_vec_R = -v_com  # assuming foot is pinned and not slipping
+
+            r_x_L = r_vec_L[0]
+            r_z_L = r_vec_L[1]
+            r_x_R = r_vec_R[0]
+            r_z_R = r_vec_R[1]
+            rdot_x_L = rdot_vec_L[0]
+            rdot_z_L = rdot_vec_L[1]
+            rdot_x_R = rdot_vec_R[0]
+            rdot_z_R = rdot_vec_R[1]
+
+            # compute the leg state in polar coordinates
+            rL = np.linalg.norm(r_vec_L)
+            rR = np.linalg.norm(r_vec_R)
+            r_hat_L = r_vec_L / rL
+            r_hat_R = r_vec_R / rR
+            rdot_L = -v_com.T @ r_hat_L
+            rdot_R = -v_com.T @ r_hat_R
+            theta_L = -np.arctan2(r_x_L, -r_z_L)
+            theta_R = -np.arctan2(r_x_R, -r_z_R)
+            thetadot_L = (r_z_L * rdot_x_L - r_x_L * rdot_z_L) / rL
+            thetadot_R = (r_z_R * rdot_x_R - r_x_R * rdot_z_R) / rR
+            
+            # pack into state vectors
+            x_left = np.array([[rL],
+                            [theta_L[0]],
+                            [rdot_L[0][0]],
+                            [thetadot_L[0]]])
+            x_right = np.array([[rR],
+                                [theta_R[0]],
+                                [rdot_R[0][0]],
+                                [thetadot_R[0]]])
+            
+        return x_left, x_right
 
 #######################################################################
 # MAIN
@@ -281,8 +405,8 @@ if __name__ == "__main__":
     # initial conditions
     x0_com = np.array([[0.25], # px [m]
                        [0.75], # py [m]
-                       [1.0],  # vx [m/s]
-                       [5.0]]) # vy [m/s]
+                       [0],  # vx [m/s]
+                       [0]]) # vy [m/s]
     x0_left = np.array([[0.0],  # r [m]
                         [0.0], # theta[rad]
                         [0.0],  # rdot [m/s]
@@ -300,11 +424,11 @@ if __name__ == "__main__":
 
     # run the simulation
     t0 = time.time()
-    t, x = mdrom.RK3_rollout(x0_com, x0_left, x0_right, p0_feet, U, D0)
+    t, x_com, x_left, x_right = mdrom.RK3_rollout(x0_com, x0_left, x0_right, p0_feet, U, D0)
     tf = time.time()
     print('Simulation time: ', tf - t0)
 
-    # # run the simulation
+    # # # run the simulation
     # sims = 500
     # DT = []
     # for i in range(0, sims):
@@ -319,14 +443,48 @@ if __name__ == "__main__":
     # print('Average simulation time: ', np.mean(DT))
     # print('Frequency: ', np.mean(1/DT))
     
+    # plot the center of mass trajcetory
     plt.figure()
     plt.plot(0, 0, 'ko', label='ground')    
-    plt.plot(x[0, :], x[1, :], label='x')
+    plt.plot(x_com[0, :], x_com[1, :], label='x')
     plt.plot(x0_com[0], x0_com[1], 'go', label='x0')
-    plt.plot(x[0, -1], x[1, -1], 'rx', label='xf')
+    plt.plot(x_com[0, -1], x_com[1, -1], 'rx', label='xf')
     plt.xlabel('x [m]')
     plt.ylabel('y [m]')
     plt.grid()
     plt.axis('equal') 
     plt.legend()
+    plt.show()
+
+    # plot the leg states
+    plt.figure()
+
+    plt.subplot(2, 2, 1)
+    plt.plot(t, x_left[0, :], label='left')
+    plt.plot(t, x_right[0, :], label='right')
+    plt.xlabel('time [s]')
+    plt.ylabel('r [m]')
+    plt.legend()
+
+    plt.subplot(2, 2, 2)
+    plt.plot(t, x_left[1, :], label='left')
+    plt.plot(t, x_right[1, :], label='right')
+    plt.xlabel('time [s]')
+    plt.ylabel('theta [rad]')
+    plt.legend()
+
+    plt.subplot(2, 2, 3)
+    plt.plot(t, x_left[2, :], label='left')
+    plt.plot(t, x_right[2, :], label='right')
+    plt.xlabel('time [s]')
+    plt.ylabel('rdot [m/s]')
+    plt.legend()
+
+    plt.subplot(2, 2, 4)
+    plt.plot(t, x_left[3, :], label='left')
+    plt.plot(t, x_right[3, :], label='right')
+    plt.xlabel('time [s]')
+    plt.ylabel('thetadot [rad/s]')
+    plt.legend()
+    
     plt.show()
