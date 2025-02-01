a040ead2f5b56538c309be02b95be2fdcfd17af3
sesteban951 <sesteban@caltech.edu>
2025-01-31 23:44:45 -0800

    fwd and inv kinematics of the leg

--- matlab/3D/rotations.m
+++ matlab/3D/rotations.m
@@ -4,33 +4,41 @@
 clear all; clc; close all;
 
 % extrinsic rotation angles
-theta_x = 1.0;
-theta_y = 1.0;
+theta_x_lims = [-80, 80];
+theta_y_lims = [-80, 80];
+theta_x_deg = unifrnd(theta_x_lims(1), theta_x_lims(2));
+theta_y_deg = unifrnd(theta_y_lims(1), theta_y_lims(2));
+% theta_x_deg = 45;
+% theta_y_deg = 45;
+theta_x = deg2rad(theta_x_deg);
+theta_y = deg2rad(theta_y_deg);
 
-% extrinsic rotation angles
+% compute the absolute angle
 L = 1;
 r0 = [0, 0, -L];
-% R_pitch_ = R_pitch(y_angle);
-% R_roll_ = R_roll(x_angle);
-% r_rot = R_roll_ * R_pitch_ * r0';
-r_z = -sqrt(L^2 / (tan(theta_x)^2 + tan(theta_y)^2 + 1));
-r_x = r_z * tan(theta_y);
-r_y = -r_z * tan(theta_x);
-r_rot = [r_x, r_y, r_z]
+rz = -sqrt(L^2 / (tan(theta_x)^2 + tan(theta_y)^2 + 1));
+rx = rz * tan(theta_y);
+ry = -rz * tan(theta_x);
+r_rot = [rx, ry, rz];
 
-% find the absolute angles 
-% theta_y = 
-% theta_x = 
-% theta_x = rad2deg(theta_x);
-% theta_y = rad2deg(theta_y);
+% projections on the axes
+r_theta_yz = [0, sin(theta_x), -cos(theta_x)];
+r_theta_xz = [-sin(theta_y), 0, -cos(theta_y)];
 
-% x_angle = rad2deg(x_angle);
-% y_angle = rad2deg(y_angle);
 
-% disp(['theta_x_extrinsic: ', num2str(x_angle)]);
-% disp(['theta_y_extrinsic: ', num2str(x_angle)]);
-% disp(['theta_x: ', num2str(theta_x)]);
-% disp(['theta_y: ', num2str(theta_y)]);
+% display the results
+disp("random theta_x =")
+disp(theta_x)
+disp("random theta_y =")
+disp(theta_y)
+
+theta_x_computed = atan2(ry, -rz);
+theta_y_computed = -atan2(rx, -rz);
+
+disp("computed theta_x =")
+disp(theta_x_computed)
+disp("computed theta_y =")
+disp(theta_y_computed)
 
 % plot it in 3D
 figure;
@@ -48,28 +56,14 @@ plot3([0, r0(1)], [0, r0(2)], [0, r0(3)], 'k--', 'LineWidth', 2);
 % plot rotated vector
 plot3([0, r_rot(1)], [0, r_rot(2)], [0, r_rot(3)], 'm', 'LineWidth', 2);
 
+% plot projections
+plot3([0, r_theta_yz(1)], [0, r_theta_yz(2)], [0, r_theta_yz(3)], 'k:', 'LineWidth', 2);
+plot3([0, r_theta_xz(1)], [0, r_theta_xz(2)], [0, r_theta_xz(3)], 'k:', 'LineWidth', 2);
+
+msg = sprintf('theta_x = %.3f\ntheta_y = %.3f', theta_x_deg, theta_y_deg);
+title(msg);
+
 xlabel('X');
 ylabel('Y');
 zlabel('Z');
 view(3);
-
-% % extrinsic rotation angles
-% function R_z = R_yaw(angle)
-%     R_z = [1, 0,           0;
-%            0, cos(angle), -sin(angle);
-%            0, sin(angle),  cos(angle)];
-% end
-
-% % extrinsic rotation angles
-% function R_y = R_pitch(angle)
-%     R_y = [cos(angle),  0, sin(angle);
-%            0,           1, 0;
-%           -sin(angle),  0, cos(angle)];
-% end
-
-% % extrinsic rotation angles
-% function R_x = R_roll(angle)
-%     R_x = [cos(angle), -sin(angle), 0;
-%            sin(angle),  cos(angle), 0;
-%            0,           0,          1];
-% end
